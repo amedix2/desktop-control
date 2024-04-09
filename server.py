87a3f328af9dc5c421de3f309b26790f9a1e51d6
amedix <amedix2@gmail.com>
2024-04-10 03:01:54 +0400

    added real-time view

--- server.py
+++ server.py
@@ -2,21 +2,23 @@ import numpy as np
 import cv2
 import pyautogui
 import socket
+import time
+# import keyboard
 
 
 def screen(size_x: int = 1280, size_y: int = 720) -> np.ndarray:
     image = pyautogui.screenshot()
     image = cv2.cvtColor(np.array(image),
-                         cv2.COLOR_RGB2BGR),
-    image = cv2.resize(image, (size_x, size_y,), )
+                         cv2.COLOR_RGB2BGR)
+    image = cv2.resize(image, (size_x, size_y))
     return image
 
 
 class Sock:
-    def __init__(self, host: str, port: int) -> None:
+    def __init__(self, host: str, port: int, listen: int) -> None:
         self.s = socket.socket()
         self.s.bind((host, port))
-        self.s.listen(1)
+        self.s.listen(listen)
         self.conn, self.addr = self.s.accept()
         print('connected:', self.addr)
 
@@ -29,9 +31,18 @@ class Sock:
 
 
 if __name__ == '__main__':
-    s = Sock('0.0.0.0', 9999)
-    s.send_data(b'hello')
-    s.close()
+    # cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
+    # cv2.setWindowProperty('screen', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    while True:
+        img = screen()
+        cv2.imshow('screen', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+    # s = Sock('0.0.0.0', 9999, 1)
+    # s.send_data(b'hello')
+    # s.close()
 
     # import time
     #
