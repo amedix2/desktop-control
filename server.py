beebe02e6b60dc71473859b2509a915b949f26d2
amedix <amedix2@gmail.com>
2024-04-09 13:00:09 +0400

    send_data init

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-import pyautoguis
+import pyautogui
 
 
 def screen(size_x: int = 1280, size_y: int = 720):
@@ -11,10 +11,16 @@ def screen(size_x: int = 1280, size_y: int = 720):
     return image
 
 
+def send_data(data: numpy.ndarray):
+    ...
+
+
 if __name__ == '__main__':
     import time
+
     start_time = time.time()
     img = screen()
     print(time.time() - start_time)
+    print(type(img))
     cv2.imshow('screen', img)
     cv2.waitKey(0)
